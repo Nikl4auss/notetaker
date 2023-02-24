534d35182cf10212747db6d5b259d8833f438010
Nicolas Marinelli <nicolasmarinelli@protonmail.com>
2023-02-24 06:54:26 -0300

    Created the Content component for creating Topics

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,9 @@
+import { useState } from "react";
 import { type NextPage } from "next";
+import { useSession } from "next-auth/react";
 import Head from "next/head";
 
+import { api, RouterOutputs } from "~/utils/api";
 import { Header } from "~/components/Header";
 
 const Home: NextPage = () => {
@@ -12,10 +15,72 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <Header></Header>
+        <Header  />
+        <Content />
       </main>
     </>
   );
 };
 
 export default Home;
+
+type Topic = RouterOutputs["topic"]["getAll"][0]
+
+const Content: React.FC = () => {
+
+  const { data: sessionData } = useSession()
+
+  const [ selectedTopic, setSelectedTopic] = useState<Topic | null>(null)
+
+  const { data: topics, refetch: refetchTopics } = api.topic.getAll.useQuery(
+    undefined,
+    {
+      enabled: sessionData?.user !== undefined,
+      onSuccess: (data) => {
+        setSelectedTopic(selectedTopic ?? data[0] ?? null)
+      }
+    }
+  )
+
+  const createTopic = api.topic.create.useMutation({
+    onSuccess: () => {
+      void refetchTopics()
+    }
+  })
+  return (
+    <div className="mx-5 mt-5 grid grid-cols-4 gap-2">
+      <div className="px-2">
+        <ul className="menu rounded-box w-56 bg-base-100 p-2">
+          { topics?.map((topic) => (
+            <li key={topic.id}>
+              <a 
+              href="#"
+              onClick={(evt) => {
+                evt.preventDefault()
+                setSelectedTopic(topic)
+              }}
+              >
+                {topic.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+        <div className="divider"></div>
+        <input 
+          type="text"
+          placeholder="New Topic"
+          className="input-bordered input input-sm w-full"
+          onKeyDown={(e) => {
+            if (e.key === "Enter"){
+              createTopic.mutate({
+                title: e.currentTarget.value
+              })
+              e.currentTarget.value = ""
+            }
+          }} 
+        />
+      </div>
+      <div className="col-span-3"></div>
+    </div>
+  )
+}
