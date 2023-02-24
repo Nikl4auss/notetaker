8ee9346282571b8ec204bf0d966eba44c26c895e
Nicolas Marinelli <nicolasmarinelli@protonmail.com>
2023-02-24 06:33:46 -0300

    Created the _document file

---
+++ src/pages/_document.tsx
@@ -0,0 +1,13 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+    return (
+        <Html data-theme="winter">
+            <Head></Head>
+            <body>
+                <Main></Main>
+                <NextScript></NextScript>
+            </body>
+        </Html>
+    )
+}
