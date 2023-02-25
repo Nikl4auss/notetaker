dd9ef2c0903333a1a312b73c9a71929b2a552542
Nicolas Marinelli <nicolasmarinelli@protonmail.com>
2023-02-25 08:53:44 -0300

    Modified the NoteCard to receive onEdit function as prop, and made it use the Modal component when editing a note

--- src/components/NoteCard.tsx
+++ src/components/NoteCard.tsx
@@ -4,39 +4,64 @@ import ReactMarkdown from 'react-markdown'
 
 import { type RouterOutputs } from '~/utils/api'
 
+import Modal from './Modal'
+import { NoteEditor } from './NoteEditor'
+
 type Note = RouterOutputs['note']['getAll'][0]
 
 export const NoteCard = ({
     note,
     onDelete,
     onEdit
-}:{
+}: {
     note: Note
     onDelete: () => void
-    onEdit: () => void
+    onEdit: (note: { title: string, content: string }) => void
 }) => {
     const [isExpanded, setIsExpanded] = useState<boolean>(true)
 
+    const [openEditModal, setOpenEditModal] = useState<boolean>(false)
+
     return (
-        <div className='card mt-5 border border-gray-200 bg-base-100 shadow-xl'>
-            <div className='card-body m-0 p-3'>
-                <div
-                    className={`collapse-arrow 
-                    ${isExpanded ? 'collapse-open' : ''} 
-                    collapse`}
-                    
-                >
-                    <div className='collapse-title text-xl font-bold hover:cursor-pointer' onClick={() => setIsExpanded(!isExpanded)}>{note.title}</div>
-                    <div className='collapse-content'>
-                        <article className='prose lg:prose-xl'>
-                            <ReactMarkdown>{note.content}</ReactMarkdown>
-                        </article>
+        <>
+            <div className='card mt-5 border border-gray-200 bg-base-100 shadow-xl'>
+                <div className='card-body m-0 p-3'>
+                    <div
+                        className={`collapse-arrow 
+                        ${isExpanded ? 'collapse-open' : ''} 
+                        collapse`}
+                    >
+                        <div className='collapse-title text-xl font-bold hover:cursor-pointer' onClick={() => setIsExpanded(!isExpanded)}>{note.title}</div>
+                        <div className='collapse-content'>
+                            <article className='prose lg:prose-xl'>
+                                <ReactMarkdown>{note.content}</ReactMarkdown>
+                            </article>
+                        </div>
                     </div>
                 </div>
+                <div className='card-actions mx-2 flex justify-end mb-2'>
+                    <button
+                        className='btn-info btn-xs btn px-7 py-3 text-sm place-content-center'
+                        onClick={() => setOpenEditModal(true)}
+                    >Edit</button>
+                    <button className='btn-warning btn-xs btn px-7 py-3 text-sm place-content-center' onClick={onDelete}>Delete</button>
+                </div>
             </div>
-            <div className='card-actions mx-2 flex justify-end mb-1'>
-                <button className='btn-warning btn-xs btn px-7 py-3 text-sm place-content-center' onClick={onDelete}>Delete</button>
-            </div>
-        </div>
+            {openEditModal
+                ? (
+                    <Modal
+                        isOpen={openEditModal}
+                        closeModal={setOpenEditModal}
+                    >
+                        <NoteEditor
+                            note={note}
+                            onSave={({ title, content }) => {
+                                void onEdit({ title, content })
+                                void setOpenEditModal(false)
+                            }} />
+                    </Modal>)
+                : null
+            }
+        </>
     )
 }
