b5f49cf29affe95595332e8021b1e07eb78eccef
Nicolas Marinelli <nicolasmarinelli@protonmail.com>
2023-02-27 02:48:02 -0300

    Added the Image component to remplace the img tag

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 import { signIn, signOut, useSession } from "next-auth/react";
-
+import Image from "next/image";
 export const Header = () => {
     const { data: sessionData } = useSession()
 
@@ -21,9 +21,11 @@ export const Header = () => {
                         onClick={() => void signOut()}
                     >
                         <div className="w-10 rounded-full">
-                            <img 
+                            <Image 
                             src={sessionData?.user?.image ?? ""} 
-                            alt={sessionData?.user?.name ?? ""} 
+                            alt={sessionData?.user?.name ?? ""}
+                            width={50}
+                            height={50}
                             />
                         </div>
                     </label>
